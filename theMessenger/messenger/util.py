7220bfea86de6aff6d90c9118cf81cc33376ee06
YFurtado3g <lucas_shiguioka@hotmail.com>
2020-05-19 21:39:18 -0300

    added crypto

--- theMessenger/messenger/util.py
+++ theMessenger/messenger/util.py
@@ -1,5 +1,6 @@
 ##util package
 from .models import AuthUser
+import hashlib
 
 class newUser:
 
@@ -13,7 +14,7 @@ class newUser:
         return{
             "username" : self.username,
             "email" :self.username,
-            "password" : self.password,
+            "password" : self.crypto(self.password),
             "first_name" : self.first_name,
             "last_name":self.last_name,
             "is_superuser":False,
@@ -21,3 +22,10 @@ class newUser:
             "is_active":True
         }
        
+    def crypto(self,password):
+        return hashlib.pbkdf2_hmac(
+                'sha256', # The hash digest algorithm for HMAC
+                password.encode('utf-8'), # Convert the password to bytes
+                100000, # It is recommended to use at least 100,000 iterations of SHA-256 
+                dklen=128 # Get a 128 byte key
+            )
